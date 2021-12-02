ea93c582307778a76f85caf4a3453ca54f9338a3
zorro27 <73430357+zorro27@users.noreply.github.com>
2021-12-02 22:07:14 +0300

    Initial Commit

---
+++ BlackStarProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlackStarProject
+//
+//  Created by Роман Зобнин on 02.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
